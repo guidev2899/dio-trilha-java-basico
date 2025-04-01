09401ec19387ff4d34321b0cad2a4083e9a8b888
guiav2899 <guilherme.dev2899@gmail.com>
2025-04-01 10:58:01 -0300

    Criação da logica de negocios

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+
+        Integer conta = 1000 +  random.nextInt(9000);
+
+        System.out.println("Digite o seu nome: ");
+        String nome = input.nextLine();
+
+        System.out.println("Digite o número da sua agência: ");
+        String agencia = input.nextLine();
+
+        System.out.println("Digite seu saldo: ");
+        Float saldo = input.nextFloat();
+
+
+
     }
 }
